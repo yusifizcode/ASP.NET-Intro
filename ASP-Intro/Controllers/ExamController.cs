40b3730e45ea17cff70d708e895e1b9c9d50814e
yusifizcode <5ba0gh2@code.edu.az>
2022-05-27 11:19:37 +0400

    asp.net introduztion

---
+++ ASP-Intro/Controllers/ExamController.cs
@@ -0,0 +1,40 @@
+﻿using ASP_Intro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Intro.Controllers
+{
+    public class ExamController : Controller
+    {
+
+        private List<Exam> _exams;
+        public ExamController()
+        {
+            _exams = new List<Exam>
+            {
+                new Exam(1,"ayten magmin",1),
+                new Exam(2,"yusif salak",100)
+            };
+        }
+        public ActionResult Index(int id)
+        {
+            Exam exam = _exams.Find(x=>x.Id==id);
+            ViewBag.ExamId = exam.Id;
+            ViewBag.FullName = exam.StudentFullName;
+            return View();
+        }
+
+
+        public ActionResult Detail(int id)
+        {
+            Exam exam = _exams.Find(x => x.Id == id);
+            ViewBag.ExamId = exam.StudentFullName;
+            ViewBag.Point = exam.Point;
+            return View();
+        }
+
+    }
+}
---
+++ ASP-Intro/Models/Exam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Intro.Models
+{
+    public class Exam
+    {
+        public Exam(int id, string fullname,int point)
+        {
+            this.Id = id;
+            this.StudentFullName = fullname;
+            this.Point = point;
+        }
+        public int Id { get; set; }
+        public string StudentFullName { get; set; }
+        public int Point { get; set; }
+    }
+}
